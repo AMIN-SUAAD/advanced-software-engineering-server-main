6e93c4fedd0bead9d050608d1cdf8dd3f7e6c387
Amin-Suaad <suaad.eee.aust@gmail.com>
2022-02-11 12:34:05 +0100

    changes

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
-app.get('/', (req, res) => {
-  res.send('hello')
-})
+
 
 client.connect((err) => {
   const orderCollection = client.db("ema-john").collection("orderCollection");
